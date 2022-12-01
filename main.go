6d83c16d8c129416bbab9d921a171a4e270cb01f
Soufiane Boursen <dev.sboursen@gmail.com>
2022-12-01 19:47:47 +0100

    Set up the database driver with pgx

--- main.go
+++ main.go
@@ -2,8 +2,17 @@ package main
 
 import (
 	"fmt"
+	"log"
+
+	"github.com/Sboursen/ginrestapi/models"
 )
 
 func main() {
 	fmt.Println("Hello")
+
+	db, err := models.Database()
+	if err != nil {
+		log.Println(err)
+	}
+	db.DB()
 }
